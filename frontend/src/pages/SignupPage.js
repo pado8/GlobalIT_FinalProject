107a09784adb2dcab299b547b187768633bed463
첼리 <kutochess1@gmail.com>
2025-06-10 17:40:42 +0900

    update: SignupPage css

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "../pages/SignupPage.css";
+import logo from "../assets/img/kickauction_logo.png";
+import socialg from "../assets/img/social_g.png";
+import socialk from "../assets/img/social_k.png";
 
 function SignupPage() {
   const [email, setEmail] = useState("");
@@ -11,41 +14,52 @@ function SignupPage() {
   return (
     <div className="signup_container">
       <div className="signup_smallcontainer">
-        <h1 style={{ textAlign: "center", marginBottom: "1.5rem", color: "#3fb6be", fontSize: "2.7rem" }}>Welcome !</h1>
-        <h2 style={{ textAlign: "center", marginBottom: "1.5rem", fontSize: "1.25rem", fontWeight: "500" }}>일반유저 회원가입</h2>
+        <img src={logo} alt="킥옥션 로고" className="signup_logo" />
+        <h2 style={{ textAlign: "center", marginBottom: "0.75rem", fontSize: "1.25rem", fontWeight: "500" }}>일반 유저 회원가입</h2>
+        <button type="button" className="company_signup">
+          ↩ ㅤ업체 가입하기
+        </button>
 
         <form className="signup_form">
-          <input type="email" className="login_input input" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <button type="button" className="email_duplicheck">
-            중복확인
-          </button>
+          <div className="signup_input_container">
+            <input type="email" className="email_input input" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <button type="button" className="email_duplicheck">
+              중복확인
+            </button>
+          </div>
 
-          <input type="text" className="nickname_input input" placeholder="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)} required />
-          <button type="button" className="nickname_duplicheck">
-            중복확인
-          </button>
+          <div className="signup_input_container">
+            <input type="text" className="nickname_input input" placeholder="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)} required />
+            <button type="button" className="nickname_duplicheck">
+              중복확인
+            </button>
+          </div>
 
-          <input type="password" className="password_input input" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          <input type="password" className="password_check_input input" placeholder="비밀번호 재입력" value={confirmPw} onChange={(e) => setConfirmPw(e.target.value)} required />
+          <div className="signup_input_container">
+            <input type="password" className="password_input input" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          </div>
+          <div className="signup_input_container">
+            <input type="password" className="password_check_input input" placeholder="비밀번호 재입력" value={confirmPw} onChange={(e) => setConfirmPw(e.target.value)} required />
+          </div>
 
-          <input type="tel" className="tel_input input" placeholder="전화번호" value={phone} onChange={(e) => setPhone(e.target.value)} required />
-          <button type="button" className="check_button">
-            인증받기
-          </button>
-
-          <button type="button" className="company_signup">
-            업체 회원가입
-          </button>
-
-          <button type="button" className="social_signup_k">
-            카카오 계정으로 회원가입
-          </button>
-          <button type="button" className="social_signup_g">
-            구글 계정으로 회원가입
-          </button>
+          <div className="signup_input_container">
+            <input type="tel" className="tel_input input" placeholder="전화번호" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+            <button type="button" className="check_button">
+              인증받기
+            </button>
+          </div>
 
           <button type="submit" className="login_button">
             가입하기
+          </button>
+
+          <button type="button" className="social_signup_k">
+            <img class="socialicon" src={socialk}></img>
+            카카오 계정으로 회원가입
+          </button>
+          <button type="button" className="social_signup_g">
+            <img class="socialicon" src={socialg}></img>
+            구글 계정으로 회원가입
           </button>
         </form>
       </div>
