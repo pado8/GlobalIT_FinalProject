86c8541e6f0479db37dc16e41990fe843eee46f9
dbwogns1 <zxcoos@naver.com>
2025-06-24 16:11:33 +0900

    feat:기본 이미지 생성 로직 수정

--- backend/kickauction/src/main/java/com/sports/kickauction/config/DefaultImageInitializer.java
+++ backend/kickauction/src/main/java/com/sports/kickauction/config/DefaultImageInitializer.java
@@ -16,28 +16,35 @@ public class DefaultImageInitializer {
     @Value("${upload.path}")
     private String uploadPath;
 
+    private final String DEFAULT_IMAGE_NAME = "default.png";
+    private final String BASE_PROFILE_IMAGE_NAME = "baseprofile.png";
+
     @PostConstruct
     public void init() throws IOException {
-        // 1. default.png → /upload/default/
         Path defaultDir = Paths.get(uploadPath, "default");
         if (!Files.exists(defaultDir)) {
             Files.createDirectories(defaultDir);
         }
-        copyIfNotExists("default.png", defaultDir);
 
-        // 2. baseprofile.png → /upload/
-        Path uploadRoot = Paths.get(uploadPath);
-        copyIfNotExists("baseprofile.png", uploadRoot);
-    }
-
-    private void copyIfNotExists(String fileName, Path targetDir) throws IOException {
-        Path target = targetDir.resolve(fileName);
-        if (!Files.exists(target)) {
-            ClassPathResource resource = new ClassPathResource("static/" + fileName);
-            Files.copy(resource.getInputStream(), target);
-            System.out.println("[기본 이미지] " + fileName + " 복사 완료 → " + target.toString());
-        } else {
-            System.out.println("[기본 이미지] " + fileName + " 이미 존재함");
+        Path pfDir = Paths.get(uploadPath);
+        if (!Files.exists(pfDir)) {
+            Files.createDirectories(pfDir);
         }
+
+        copyIfNotExists(DEFAULT_IMAGE_NAME, defaultDir);
+
+        copyIfNotExists(BASE_PROFILE_IMAGE_NAME, pfDir);
+        
     }
+
+        private void copyIfNotExists(String fileName, Path targetDir) throws IOException {
+            Path target = targetDir.resolve(fileName);
+            if (!Files.exists(target)) {
+                ClassPathResource resource = new ClassPathResource("static/" + fileName);
+                Files.copy(resource.getInputStream(), target);
+                System.out.println("[기본 이미지] " + fileName + " 복사 완료");
+            } else {
+                System.out.println("[기본 이미지] " + fileName + " 이미 존재함");
+            }
+        }
 }
