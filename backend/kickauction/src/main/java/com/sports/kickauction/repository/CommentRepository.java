94a0d48953d0689a4f1218702edd6effadc10e6d
pado8 <floe9235@gmail.com>
2025-06-24 17:19:42 +0900

    edit : 댓글 수정 삭제 기능 추가 완료

--- backend/kickauction/src/main/java/com/sports/kickauction/repository/CommentRepository.java
+++ backend/kickauction/src/main/java/com/sports/kickauction/repository/CommentRepository.java
@@ -1,10 +1,14 @@
 package com.sports.kickauction.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sports.kickauction.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 게시글(pno)의 댓글을 최신순으로 조회
     List<Comment> findByPnoOrderByCregdateDesc(Long pno);
+
+    Optional<Comment> findByCnoAndPno(Long cno, Long pno);
 }
--- backend/kickauction/src/main/java/com/sports/kickauction/service/CommentService.java
+++ backend/kickauction/src/main/java/com/sports/kickauction/service/CommentService.java
@@ -10,4 +10,8 @@ public interface CommentService {
 
     // 로그인 사용자만 호출
     CommentDTO writeComment(Long pno, CommentDTO dto, Authentication auth);
+
+    CommentDTO updateComment(Long pno, Long cno, String content, Authentication auth);
+
+    void deleteComment(Long pno, Long cno, Authentication auth);
 }
