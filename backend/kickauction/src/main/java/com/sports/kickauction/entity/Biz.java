89d5995ff874185f2e88c29a9d0331ce3a4fb2e5
dbwogns1 <zxcoos@naver.com>
2025-06-26 16:04:34 +0900

    add: biz 추가

--- backend/kickauction/src/main/java/com/sports/kickauction/entity/Biz.java
+++ backend/kickauction/src/main/java/com/sports/kickauction/entity/Biz.java
@@ -1,44 +1,44 @@
-package com.sports.kickauction.entity;
+// package com.sports.kickauction.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+// import lombok.ToString;
 
-@Entity
-@Table(name = "biz")
-@Getter
-@Setter
-@ToString(exclude = {"member", "order"}) // 순환참조 방지
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor 
-public class Biz extends BaseEntity {
+// @Entity
+// @Table(name = "biz")
+// @Getter
+// @Setter
+// @ToString(exclude = {"member", "order"}) // 순환참조 방지
+// @Builder
+// @AllArgsConstructor
+// @NoArgsConstructor 
+// public class Biz extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long bno;
+//   @Id
+//   @GeneratedValue(strategy = GenerationType.IDENTITY)
+//   private Long bno;
 
-  @ManyToOne
-  @JoinColumn(name = "mno", nullable = false)
-  private Seller seller;
+//   @ManyToOne
+//   @JoinColumn(name = "mno", nullable = false)
+//   private  seller;
 
-  @ManyToOne
-  @JoinColumn(name = "ono", nullable = false)
-  private Order order;
+//   @ManyToOne
+//   @JoinColumn(name = "ono", nullable = false)
+//   private Order order;
 
-  private int price;
+//   private int price;
 
-  private String bcontent;
+//   private String bcontent;
 
-  private String banswer;
-}
+//   private String banswer;
+// }
 
